b19a174e304ba227a3012787f7ec662c061bbf1b
taufiqalif <aliftaufiq4@gmail.com>
2022-05-10 03:45:02 +0700

    new update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # PHP Dasar
+
+## langkah - langkah praktikum 7
+
+1. membuat file `lab7web.php`
+
+2. membuat document HTML
+
+3. membuat form
+
+![01.png](img/01.png)
+
+4. menjalankan fungsi form
+
+![02.png](img/02.png)
+
+5. hasil
+
+![03.png](img/03.png)
--- lab7web.php
+++ lab7web.php
@@ -5,30 +5,40 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <title>PHP Dasar</title>
 </head>
 
 <body>
-  <form action="" method="POST">
-    <label for="">Nama</label>
-    <input type="text" name="nama" id="">
-    <br>
-    <label for="">tanggal lahir</label>
-    <input type="date" name="tanggal" value="">
-    <br>
-    <label for="job">pekerjaan</label>
-    <select name="job" id="job">
-      <option value="">Pilih...</option>
-      <option value="">Programmer</option>
-      <option value="">Designer</option>
-      <option value="">Manager</option>
-    </select>
-    <br>
-    <input type="submit" name="submit" value="Submit">  
-  </form>
+
+  <div class="kotak">
+    <p class="biodata">Isi biodata</p>
+
+    <form action="" method="POST">
+      <label for="">Nama</label>
+      <input class="nama" type="text" name="nama" id="">
+      <br>
+      <label for="">tanggal lahir</label>
+      <input class="nama" type="date" name="tanggal" value="">
+      <br>
+      <label for="job">pekerjaan</label>
+      <select class="nama" name="job" id="job">
+        <option value="pilih">Pilih...</option>
+        <option value="programer">Programmer</option>
+        <option value="designer">Designer</option>
+        <option value="manager">Manager</option>
+      </select>
+      <br>
+      <input type="submit" class="tombol" name="submit" value="Submit">  
+    </form>
+  
+  <div class="output">
   <?php 
-    echo 'nama' . $_POST['nama'];
+    // nama
+    echo 'nama saya ' . $_POST['nama'];
     echo '<br>';
+
+    // tanggal lahir
     $tanggal_lahir = new DateTime($_POST['tanggal']);
     $sekarang = new DateTime("today");
     if ($tanggal_lahir > $sekarang) { 
@@ -39,9 +49,24 @@
     $thn = $sekarang->diff($tanggal_lahir)->y;
     $bln = $sekarang->diff($tanggal_lahir)->m;
     $tgl = $sekarang->diff($tanggal_lahir)->d;
-    echo "Umur anda adalah :<br>";
-    echo $thn." tahun ".$bln." bulan ".$tgl." hari";
+    echo "Umur saya  " . $thn . " tahun " . $bln . " bulan " . $tgl . " hari";
+    echo '<br>';
+
+    // pekerjaan
+    echo 'pekerjaan pekerjaan saya ' . $_POST['job'];
+    echo '<br>';
+    if ($_POST['job'] == 'programer') {
+      echo 'gaji saya 12 juta';
+    } elseif ($_POST['job'] == 'designer') {
+      echo 'gaji saya 10 juta';
+    } elseif ($_POST['job'] == 'manager') {
+      echo 'gaji saya 8 juta';
+    } else {
+      echo 'anda tidak punya pekerjaan';
+    }
   ?>
+  </div>
+  </div>
 </body>
 
 </html>
